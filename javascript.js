dbfb5512896bd123cd002ca41b270ab4a94fac60
LuxScintilla <catherina.ea@gmail.com>
2023-02-10 09:27:13 +0000

    Add working code up to loop

--- javascript.js
+++ javascript.js
@@ -1,27 +1,55 @@
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    const CHOICES = ["rock", "paper", "scissors"];
-    return CHOICES[randomNumber];
-    // console.log(CHOICES[randomNumber])
-}
+function playGame() {
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Your weapon of choice:", "Rock, Paper, or Scissors");
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
-    // console.log(playerChoice)
-}
-
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-
-console.log(computerSelection)
-console.log(playerSelection)
-
-function playGame(computerSelection, playerSelection) {
-
-}
-
-let result = playGame();
-
-console.log(playGame)
+    let computerSelection = (function() {
+        const choices = ["rock", "paper", "scissors"]
+        let randomNumber = Math.floor(Math.random() * 3);
+        return choices[randomNumber];
+    })();
+    
+    console.log(computerSelection)
+      
+    let playerSelection = (function() {
+        let playerChoice = prompt("Your weapon of choice:", "rock, paper, scissors");
+        return playerChoice.toLowerCase();
+    })();
+    
+    console.log(playerSelection)
+    
+    function playRound(computerSelection, playerSelection) {
+        if((computerSelection === "rock") && (playerSelection === "paper")){
+         return "Player Wins!"   
+        } else if((computerSelection === "paper") && (playerSelection === "scissors")) {
+         return "Player Wins!"   
+        } else if((computerSelection === "scissors") && (playerSelection === "rock")) {
+         return "Player Wins!"   
+        } else if((computerSelection === "rock") && (playerSelection === "scissors")) {
+         return "Computer Wins!"   
+        } else if((computerSelection === "scissors") && (playerSelection === "paper")) {
+         return "Computer Wins!"   
+        } else if((computerSelection === "paper") && (playerSelection === "rock")) {
+         return "Computer Wins!"   
+        } else if((computerSelection === "rock") && (playerSelection === "rock")) {
+         return "It is a Draw!"   
+        } else if((computerSelection === "paper") && (playerSelection === "paper")) {
+         return "It is a Draw!"   
+        } else if((computerSelection === "scissors") && (playerSelection === "scissors")) {
+         return "It is a Draw!"   
+        } else {
+        return "Something went wrong!"
+        }                 
+    }
+    
+    console.log(playRound(computerSelection, playerSelection))
+    
+    }
+    
+    
+    
+    
+        for (let rounds = 0; rounds < 5; rounds++) {
+            console.log(playGame());
+        }
+    
+    
+    
+      
