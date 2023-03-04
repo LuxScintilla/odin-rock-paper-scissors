3a5778f66e2eebfe11c3c7ccadaca29176af93eb
LuxScintilla <catherina.ea@gmail.com>
2023-03-04 22:03:01 +0000

    Full function to game UI

--- javascript.js
+++ javascript.js
@@ -1,41 +1,73 @@
+const backGround = document.querySelector(".container");
+
+backGround.addEventListener("mousemove", moveBG);
+
+function moveBG(e) {
+    backGround.style.backgroundPositionX = (e.clientX/20-200)+"px";
+
+    backGround.style.backgroundPositionY = (e.clientY/20-100)+"px";
+
+}
+
 const choiceButtons = document.querySelectorAll(".choice");
-const choiceText = document.querySelector(".choice-text");
-const roundText = document.querySelector(".round-text");
+const pChoiceText = document.querySelector(".player-choice");
+const cChoiceText = document.querySelector(".computer-choice");
+const fightButton = document.querySelector(".fight-button");
+const roundText = document.querySelector(".round-result");
+const pScoreText = document.querySelector(".player-score");
+const cScoreText = document.querySelector(".computer-score");
+const gameResultText = document.querySelector(".game-result");
+const gameReset = document.querySelector(".play-again");
 
 let pChoice;
 let cChoice;
+let roundResult;
 
 choiceButtons.forEach(function(button) {
     button.addEventListener("click", function() {
-        choiceText.textContent = button.value;
+        pChoiceText.textContent = button.value;
         pChoice = button.value;
     });
 });
 
-function computer() {
-    const choices = ["rock", "paper", "scissors"]
-    let computerChoice = choices[Math.floor(Math.random() * 3)];
-    return computerChoice;
-    }
-cChoice = computer();
+fightButton.addEventListener("click", function() {
+
+    function computer() {
+        const choices = ["rock", "paper", "scissors"]
+        let computerChoice = choices[Math.floor(Math.random() * 3)];
+        return computerChoice;
+        }
+    cChoice = computer();
+
+    playRound(pChoice, cChoice)
+    cChoiceText.textContent = cChoice;
+    keepScore()
+    pScoreText.textContent = playerScore
+    cScoreText.textContent = computerScore
+
+    buttonCount()
+});
 
 function playRound(playerSelection, computerSelection) {
     if((playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "rock")){
         roundText.textContent = "Computer Wins!"
+        roundResult = "Computer Wins!"
         return "Computer Wins!"
     }
     else if((playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")){
         roundText.textContent = "Player Wins!"
+        roundResult = "Player Wins!"
         return "Player Wins!"
     }
     else if((playerSelection === "rock" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "paper") ||
     (playerSelection === "scissors" && computerSelection === "scissors")){
         roundText.textContent = "It is a Draw!"
+        roundResult = "It is a draw!"
         return "It is a draw!"
     } else {
         roundText.textContent = "Something went wrong!"
@@ -43,67 +75,57 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
+function keepScore() {
+    if(roundResult === "Computer Wins!"){
+        computerScore++
+    } else if(roundResult === "Player Wins!"){
+        playerScore++
+    } else if(roundResult === "It is a draw!") {
+        playerScore = playerScore
+        computerScore = computerScore
+    } else if(roundResult = "Something went wrong!") {
+        playerScore = playerScore
+        computerScore = computerScore
+    } else {
+        playerScore = playerScore
+        computerScore = computerScore
+    }
+}
 
-// IGNORE THE REST OF THIS IS FOR REFACTORING
+let buttonPressed = 0;
 
-// function computer() {
-//     const choices = ["rock", "paper", "scissors"]
-//     let computerChoice = choices[Math.floor(Math.random() * 3)];
-//     return computerChoice;
-//     }
-// let cChoice = computer();
+function buttonCount () {
+    buttonPressed++
 
+    if(buttonPressed === 5) {
+        finalScore()
+    }
+}
 
-// function playRound(playerSelection, computerSelection) {
-//     if((playerSelection === "rock" && computerSelection === "paper") ||
-//     (playerSelection === "paper" && computerSelection === "scissors") ||
-//     (playerSelection === "scissors" && computerSelection === "rock")){
-//         return "Computer Wins!"
-//     }
-//     else if((playerSelection === "rock" && computerSelection === "scissors") ||
-//     (playerSelection === "paper" && computerSelection === "rock") ||
-//     (playerSelection === "scissors" && computerSelection === "paper")){
-//         return "Player Wins!"
-//     }
-//     else if((playerSelection === "rock" && computerSelection === "rock") ||
-//     (playerSelection === "paper" && computerSelection === "paper") ||
-//     (playerSelection === "scissors" && computerSelection === "scissors")){
-//         return "It is a draw!"
-//     } else {
-//         return "Something went wrong!"
-//     }
-// }
+function finalScore() {
+    if(playerScore === computerScore){
+        gameResultText.textContent = "The final result is a Draw!"
+    } else if(playerScore > computerScore){
+        gameResultText.textContent = "The winner of the game is: Player!"
+    } else if(playerScore < computerScore){
+        gameResultText.textContent = "The winner of the game is: Computer!"
+    }
+}
 
+gameReset.addEventListener("click", reset);
 
-// let playerScore = 0;
-// let computerScore = 0;
+function reset() {
+    pChoiceText.textContent = "" 
+    cChoiceText.textContent = ""
+    roundText.textContent = "Ready to Play?"
+    pScoreText.textContent = "0"
+    cScoreText.textContent = "0"
+    gameResultText.textContent = "Who will win this Round?"
+    playerScore = 0;
+    computerScore = 0;
+    buttonPressed = 0;
+}
 
-// function keepScore() {
-//     if(roundResult === "Computer Wins!"){
-//         computerScore++
-//     } else if(roundResult === "Player Wins!"){
-//         playerScore++
-//     }
-// }
-
-
-// playRound(pChoice, cChoice)
-// roundResult = playRound(pChoice, cChoice);
-// console.log(roundResult)
-// keepScore()
-// console.log(`Player Score is: ${playerScore}`)
-// console.log(`Computer Score is:${computerScore}`)
-
-
-// function finalScore() {
-//     if(playerScore === computerScore){
-//         console.log("The final result is a Draw!")
-//     } else if(playerScore > computerScore){
-//         console.log("The winner of the game is: Player!")
-//     } else if(playerScore < computerScore){
-//         console.log("The winner of the game is: Computer!")
-//     }
-// }
-
-// finalScore()
